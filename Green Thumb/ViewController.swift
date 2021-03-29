baed19f4ae2fac892d39235ce55d72525b12dd68
Jacob <mayl.jacob@gmail.com>
2021-03-29 17:21:54 -0500

    storyboard

--- Green Thumb/ViewController.swift
+++ Green Thumb/ViewController.swift
@@ -8,12 +8,52 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // global booleans acting as flags:
+    var lights_on: Bool = false;
+    var start_watering: Bool = false;
+    var automate_watering: Bool = false;
+    
+    //HOME PAGE
+    // connect device button
+    @IBOutlet weak var connect: UIButton!
+    
+    //READINGS PAGE
+    // cant get the labels to come over here...
+    @IBOutlet weak var temp_reading: UILabel!
+    //LIGHTING PAGE
 
+    @IBAction func lightswitch(_ sender: UISwitch) {
+        if(sender.isOn) {
+            lights_on = true;
+        }
+        else {
+            lights_on = false;
+        }
+    }
+    @IBAction func start_time(_ sender: Any) {
+    }
+    @IBAction func end_time(_ sender: Any) {
+    }
+    
+    //WATER PAGE
+    @IBAction func water_now(_ sender: UIButton) {
+        start_watering = true;
+        // need this to get set to false sometime.
+        //Not sure where
+    }
+    @IBAction func number_days(_ sender: Any) {
+    }
+    @IBAction func water_time(_ sender: Any) {
+    }
+    @IBAction func automate(_ sender: Any) {
+        automate_watering = true;
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
 
 }
 
